ae586c021c26845a0bb4f469b4df39995f98b439
Austin Stanley <r.austin.stanley@gmail.com>
2018-05-22 23:12:46 -0500

    Implement socket.io

--- app.js
+++ app.js
@@ -1,109 +1,109 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const low = require('lowdb')
-const FileAsync = require('lowdb/adapters/FileAsync')
+const FileSync = require('lowdb/adapters/FileSync')
 
 const app = express()
 const server = require('http').Server(app)
-var io = require('socket.io')(server)
+const io = require('socket.io')(server)
+const port = 8000;
 
 app.use(bodyParser.json())
 
-const adapter = new FileAsync('db.json')
-low(adapter)
-    .then(db => {
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
-        // GET /
-        app.get('/', (req, res) => res.send(db.get('users')))
+db.defaults({users: [
+    {
+        name: 'Grandma',
+        rsvp: true
+    },
+    {
+        name: 'Austin',
+        rsvp: true
+    },
+    {
+        name: 'Nicole',
+        rsvp: true
+    },
+    {
+        name: 'Chris',
+        rsvp: true
+    },
+    {
+        name: 'Erin',
+        rsvp: true
+    },
+    {
+        name: 'Russell',
+        rsvp: true
+    },
+    {
+        name: 'Kristin',
+        rsvp: true
+    },
+    {
+        name: 'Dale',
+        rsvp: true
+    },
+    {
+        name: 'Leanne',
+        rsvp: true
+    },
+    {
+        name: 'Jared',
+        rsvp: true
+    },
+    {
+        name: 'Allison',
+        rsvp: false
+    },
+    {
+        name: 'Medora',
+        rsvp: false
+    }
+]}).write()
 
-        // GET /users/:name
-        app.get('/users/:name', (req, res) => {
-            const user = db.get('users')
-                .find({ name: req.params.name })
-                .value()
-
-            res.send(user)
-        })
-
-        // GET /headcount
-        app.get('/headcount', (req, res) => {
-            const count = db.get('users')
-                .filter({ rsvp: true })
-                .size()
-                .value()
-
-            res.send(count + '')
-        })
-
-        // POST /
-        app.post('/', (req, res) => {
-            db.get('users')
-                .find({ name: req.body.name })
-                .assign({ rsvp: req.body.rsvp })
-                .write()
-                .then(user => res.send(user))
-        })
-
-        io.on('connection', socket => {
-            console.log('connection received from ' + socket)
-            socket.emit('handshake')
-            socket.on('getusers', data => {
-                socket.emit('getusers', db.get('users'))
-            })
-        })
-
-        return db.defaults({users: [
-            {
-                name: 'Grandma',
-                rsvp: true
-            },
-            {
-                name: 'Austin',
-                rsvp: true
-            },
-            {
-                name: 'Nicole',
-                rsvp: true
-            },
-            {
-                name: 'Chris',
-                rsvp: true
-            },
-            {
-                name: 'Erin',
-                rsvp: true
-            },
-            {
-                name: 'Russell',
-                rsvp: true
-            },
-            {
-                name: 'Kristin',
-                rsvp: true
-            },
-            {
-                name: 'Dale',
-                rsvp: true
-            },
-            {
-                name: 'Leanne',
-                rsvp: true
-            },
-            {
-                name: 'Jared',
-                rsvp: true
-            },
-            {
-                name: 'Allison',
-                rsvp: false
-            },
-            {
-                name: 'Medora',
-                rsvp: false
-            }
-        ]}).write()
+io.on('connection', socket => {
+    socket.on('ping', () => {
+        console.log('ping received')
+        socket.emit('ping')
     })
-    .then(() => {
-        //app.listen(80, () => console.log('headcount listening on port 80'))
-        server.listen(80, () => console.log('headcount listening on port 80'))
+
+    socket.on('getall', () => {
+        console.log('getall')
+        socket.emit('getall', db.get('users'))
     })
+
+    socket.on('getnames', () => {
+        console.log('getnames')
+        let names = db.get('users')
+            .map('name')
+            .value()
+
+        socket.emit('getnames', names)
+    })
+
+    socket.on('getuser', data => {
+        console.log('getuser: ' + data.name)
+        socket.emit('getuser', db.get('users').find({ name: data.name }).value())
+    })
+
+    socket.on('getheadcount', () => {
+        console.log('getheadcount')
+        socket.emit('getheadcount', db.get('users').filter({ rsvp: true }).size().value())
+    })
+
+    socket.on('rsvp', data => {
+        console.log('rsvp: ' + data.name + ', ' + data.rsvp)
+        db.get('users')
+            .find({ name: data.name })
+            .assign({ rsvp: data.rsvp })
+            .write()
+
+        socket.emit('rsvp', data)
+    })
+
+})
+
+server.listen(port, () => console.log('listening on ' + port))
